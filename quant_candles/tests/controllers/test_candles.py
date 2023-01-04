b0b576059c2874919720b27b38fcc63d06e5f2f3
Alex <globophobe@gmail.com>
2023-01-04 17:05:16 +0900

    :white_check_mark: add time based candle tests

--- quant_candles/tests/controllers/test_candles.py
+++ quant_candles/tests/controllers/test_candles.py
@@ -4,7 +4,8 @@ from unittest.mock import patch
 
 import pandas as pd
 import time_machine
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from pandas import DataFrame
 
 from quant_candles.constants import Frequency
 from quant_candles.controllers import CandleCacheIterator, aggregate_candles
@@ -14,6 +15,7 @@ from quant_candles.models import (
     CandleCache,
     CandleData,
     CandleReadOnlyData,
+    TimeBasedCandle,
     TradeData,
 )
 
@@ -23,18 +25,20 @@ from ..base import BaseSymbolTest, BaseWriteTradeDataTest
 class BaseCandleCacheIteratorTest(BaseSymbolTest):
     def setUp(self):
         super().setUp()
-        self.candle = Candle.objects.create()
+        self.candle = self.get_candle()
         self.symbol = self.get_symbol()
         self.candle.symbols.add(self.symbol)
+
+    def get_candle(self) -> Candle:
+        """Get candle."""
+        return Candle.objects.create()
 
     def get_values(self, step: str = "1d") -> List[Tuple[datetime, datetime]]:
         """Get values."""
         return [
             value
             for value in CandleCacheIterator(self.candle).iter_all(
-                timestamp_from=self.timestamp_from,
-                timestamp_to=self.timestamp_to,
-                step=step,
+                self.timestamp_from, self.timestamp_to, step
             )
         ]
 
@@ -188,18 +192,181 @@ class CandleCacheWeeklyIteratorTest(BaseCandleCacheWeeklyIteratorTest, TestCase)
         self.assertEqual(len(values), 0)
 
 
-class AggregateCandleWeeklyTest(
-    BaseWriteTradeDataTest, BaseCandleCacheWeeklyIteratorTest, TestCase
+@time_machine.travel(datetime(2009, 1, 3))
+@patch(
+    "quant_candles.controllers.iterators.CandleCacheIterator.get_max_timestamp_to",
+    return_value=datetime(2009, 1, 4).replace(tzinfo=timezone.utc),
+)
+class TimeBasedMinuteFrequencyCandleTest(
+    BaseWriteTradeDataTest, BaseCandleCacheIteratorTest, TestCase
 ):
+
     databases = {"default", "read_only"}
 
-    def test_aggregate_candles_with_weekly_cache_reset(self):
-        """Aggregate candles, with weekly cache reset."""
+    def get_candle(self) -> Candle:
+        """Get candle."""
+        return TimeBasedCandle.objects.create(json_data={"window": "1t"})
+
+    def write_trade_data(self, data_frame: DataFrame) -> None:
+        """Write trade data."""
         trade_data = TradeData(
-            symbol=self.symbol, timestamp=self.timestamp_from, frequency=Frequency.WEEK
+            symbol=self.symbol,
+            timestamp=data_frame.iloc[0].timestamp,
+            frequency=Frequency.MINUTE,
         )
-        # Save single file to speed up test.
-        TradeData.write_data_frame(
-            trade_data, self.get_filtered(self.timestamp_from), pd.DataFrame([])
+        TradeData.write_data_frame(trade_data, data_frame, pd.DataFrame([]))
+
+    def test_one_candle_from_trade_in_the_first_minute(self, mock_get_max_timestamp_to):
+        """One candle from a trade in the first minute."""
+        filtered = self.get_filtered(self.timestamp_from)
+        self.write_trade_data(filtered)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("1t")
         )
-        aggregate_candles(self.candle, self.timestamp_from, self.timestamp_to)
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 1)
+        self.assertEqual(candle_data[0].timestamp, self.timestamp_from)
+
+    @override_settings(IS_LOCAL=False)
+    def test_one_candle_from_trade_in_the_first_minute_not_read_only(
+        self, mock_get_max_timestamp_to
+    ):
+        """One candle from a trade in the first minute, which is not read only."""
+        filtered = self.get_filtered(self.timestamp_from)
+        self.write_trade_data(filtered)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("1t")
+        )
+        candle_data = CandleData.objects.all()
+        self.assertEqual(candle_data.count(), 1)
+        self.assertEqual(candle_data[0].timestamp, self.timestamp_from)
+
+    def test_two_candles_from_trades_in_the_first_and_second_minute(
+        self, mock_get_max_timestamp_to
+    ):
+        """Two candles, one in first minute, and another in the second minute."""
+        filtered_1 = self.get_filtered(self.timestamp_from)
+        filtered_2 = self.get_filtered(self.timestamp_from + pd.Timedelta("1t"))
+        self.write_trade_data(filtered_1)
+        self.write_trade_data(filtered_2)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("2t")
+        )
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 2)
+        self.assertEqual(candle_data[0].timestamp, filtered_1.iloc[0].timestamp)
+        self.assertEqual(candle_data[1].timestamp, filtered_2.iloc[0].timestamp)
+
+    def test_two_candles_from_trades_in_first_and_third_minute(
+        self, mock_get_max_timestamp_to
+    ):
+        """
+        Two candles, one in first minute, and another in the third minute.
+        No trades, no candle, for the second minute.
+        """
+        filtered_1 = self.get_filtered(self.timestamp_from)
+        TradeData.objects.create(
+            symbol=self.symbol,
+            timestamp=self.timestamp_from + pd.Timedelta("1t"),
+            frequency=Frequency.MINUTE,
+        )
+        filtered_2 = self.get_filtered(self.timestamp_from + pd.Timedelta("2t"))
+        self.write_trade_data(filtered_1)
+        self.write_trade_data(filtered_2)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("3t")
+        )
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 2)
+        self.assertEqual(candle_data[0].timestamp, filtered_1.iloc[0].timestamp)
+        self.assertEqual(candle_data[1].timestamp, filtered_2.iloc[0].timestamp)
+
+
+@time_machine.travel(datetime(2009, 1, 3))
+@patch(
+    "quant_candles.controllers.iterators.CandleCacheIterator.get_max_timestamp_to",
+    return_value=datetime(2009, 1, 4).replace(tzinfo=timezone.utc),
+)
+class TimeBasedHourFrequencyCandleTest(
+    BaseWriteTradeDataTest, BaseCandleCacheIteratorTest, TestCase
+):
+
+    databases = {"default", "read_only"}
+
+    def get_candle(self) -> Candle:
+        """Get candle."""
+        return TimeBasedCandle.objects.create(json_data={"window": "1h"})
+
+    def write_trade_data(self, data_frame: DataFrame) -> None:
+        """Write trade data."""
+        trade_data = TradeData(
+            symbol=self.symbol,
+            timestamp=data_frame.iloc[0].timestamp,
+            frequency=Frequency.HOUR,
+        )
+        TradeData.write_data_frame(trade_data, data_frame, pd.DataFrame([]))
+
+    def test_one_candle_from_trade_in_the_first_hour(self, mock_get_max_timestamp_to):
+        """One candle from a trade in the first hour."""
+        filtered = self.get_filtered(self.timestamp_from)
+        self.write_trade_data(filtered)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("1h")
+        )
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 1)
+        self.assertEqual(candle_data[0].timestamp, self.timestamp_from)
+
+    @override_settings(IS_LOCAL=False)
+    def test_one_candle_from_trade_in_the_first_hour_not_read_only(
+        self, mock_get_max_timestamp_to
+    ):
+        """One candle from a trade in the first hour, which is not read only."""
+        filtered = self.get_filtered(self.timestamp_from)
+        self.write_trade_data(filtered)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("1h")
+        )
+        candle_data = CandleData.objects.all()
+        self.assertEqual(candle_data.count(), 1)
+        self.assertEqual(candle_data[0].timestamp, self.timestamp_from)
+
+    def test_two_candles_from_trades_in_the_first_and_second_hour(
+        self, mock_get_max_timestamp_to
+    ):
+        """Two candles, one in first hour, and another in the second hour."""
+        filtered_1 = self.get_filtered(self.timestamp_from)
+        filtered_2 = self.get_filtered(self.timestamp_from + pd.Timedelta("1h"))
+        self.write_trade_data(filtered_1)
+        self.write_trade_data(filtered_2)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("2h")
+        )
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 2)
+        self.assertEqual(candle_data[0].timestamp, filtered_1.iloc[0].timestamp)
+        self.assertEqual(candle_data[1].timestamp, filtered_2.iloc[0].timestamp)
+
+    def test_two_candles_from_trades_in_first_and_third_hour(
+        self, mock_get_max_timestamp_to
+    ):
+        """
+        Two candles, one in first hour, and another in the third hour.
+        No trades, no candle, for the second hour.
+        """
+        filtered_1 = self.get_filtered(self.timestamp_from)
+        TradeData.objects.create(
+            symbol=self.symbol,
+            timestamp=self.timestamp_from + pd.Timedelta("1h"),
+            frequency=Frequency.HOUR,
+        )
+        filtered_2 = self.get_filtered(self.timestamp_from + pd.Timedelta("2h"))
+        self.write_trade_data(filtered_1)
+        self.write_trade_data(filtered_2)
+        aggregate_candles(
+            self.candle, self.timestamp_from, self.timestamp_from + pd.Timedelta("3h")
+        )
+        candle_data = CandleReadOnlyData.objects.all()
+        self.assertEqual(candle_data.count(), 2)
+        self.assertEqual(candle_data[0].timestamp, filtered_1.iloc[0].timestamp)
+        self.assertEqual(candle_data[1].timestamp, filtered_2.iloc[0].timestamp)
