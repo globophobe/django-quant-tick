3e6373ecd2f3912d7300891d1d5dbed0f671a878
Alex <globophobe@gmail.com>
2023-02-16 22:20:00 +0900

    :fire: delete unnecessary code

--- quant_candles/exchanges/bitmex/controllers.py
+++ quant_candles/exchanges/bitmex/controllers.py
@@ -1,15 +1,10 @@
-from datetime import date, datetime
-from typing import Callable, Optional
-
-import pandas as pd
-from pandas import DataFrame
+from datetime import datetime
+from typing import Callable
 
 from quant_candles.controllers import ExchangeREST, ExchangeS3, use_s3
 from quant_candles.models import Symbol
 
-from .api import get_bitmex_api_response
 from .base import BitmexRESTMixin, BitmexS3Mixin
-from .constants import API_URL
 
 
 def bitmex_trades(
@@ -47,18 +42,3 @@ class BitmexTradesREST(BitmexRESTMixin, ExchangeREST):
 
 class BitmexTradesS3(BitmexS3Mixin, ExchangeS3):
     """BitMEX trades S3."""
-
-    def get_data_frame(self, date: date) -> Optional[DataFrame]:
-        """Get data_frame.
-
-        Downloaded file has multiple symbols. Do nothing before listing date.
-        """
-        base_url = f"{API_URL}/instrument?symbol={self.symbol.api_symbol}&count=1"
-
-        def get_api_url(*args, **kwargs):
-            return base_url
-
-        data = get_bitmex_api_response(get_api_url, base_url)
-        listing = pd.to_datetime(data[0]["listing"])
-        if date >= listing.date():
-            return super().get_data_frame(date)
