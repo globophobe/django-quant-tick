22bf8fada290ccf5b3353fec46dd5b957220e118
Alex <globophobe@gmail.com>
2023-02-18 12:35:08 +0900

    :rewind: revert unnecessary change

--- quant_candles/exchanges/bitmex/controllers.py
+++ quant_candles/exchanges/bitmex/controllers.py
@@ -1,16 +1,21 @@
-from datetime import datetime
-from typing import Callable
+import datetime
+from typing import Callable, Optional
+
+import pandas as pd
+from pandas import DataFrame
 
 from quant_candles.controllers import ExchangeREST, ExchangeS3, use_s3
 from quant_candles.models import Symbol
 
+from .api import get_bitmex_api_response
 from .base import BitmexRESTMixin, BitmexS3Mixin
+from .constants import API_URL, XBTUSD
 
 
 def bitmex_trades(
     symbol: Symbol,
-    timestamp_from: datetime,
-    timestamp_to: datetime,
+    timestamp_from: datetime.datetime,
+    timestamp_to: datetime.datetime,
     on_data_frame: Callable,
     retry: bool = False,
     verbose: bool = False,
@@ -42,3 +47,26 @@ class BitmexTradesREST(BitmexRESTMixin, ExchangeREST):
 
 class BitmexTradesS3(BitmexS3Mixin, ExchangeS3):
     """BitMEX trades S3."""
+
+    def get_data_frame(self, date: datetime.date) -> Optional[DataFrame]:
+        """Get data_frame.
+
+        Downloaded file has multiple symbols. Do nothing before listing date.
+        """
+        base_url = f"{API_URL}/instrument?symbol={self.symbol.api_symbol}&count=1"
+
+        def get_api_url(*args, **kwargs):
+            return base_url
+
+        # In the case of XBTUSD, the listing date was previously correct, but has been
+        # revised by BitMEX to 2016-05-13.
+        if self.symbol.api_symbol == XBTUSD:
+            listing_date = datetime.date(2015, 9, 25)
+        else:
+            data = get_bitmex_api_response(get_api_url, base_url)
+            listing_date = pd.to_datetime(data[0]["listing"]).date()
+
+        # Without this check, empty data frames may be acquired from BitMEX data before
+        # the symbol listing data.
+        if date >= listing_date:
+            return super().get_data_frame(date)
