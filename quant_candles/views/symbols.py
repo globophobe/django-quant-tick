4b6ccc75851080ee52c350baf7f3d725e1e8f514
Alex <globophobe@gmail.com>
2023-02-19 17:55:05 +0900

    :fire: delete unnecessary views

--- quant_candles/views/symbols.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import ListAPIView
-
-from quant_candles.models import Symbol
-from quant_candles.serializers import SymbolSerializer
-
-
-class SymbolView(ListAPIView):
-    queryset = Symbol.objects.all()
-    serializer_class = SymbolSerializer
