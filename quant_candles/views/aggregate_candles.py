10929bf7b03dd554ac41786a46918b74aeeecdf6
Alex <globophobe@gmail.com>
2023-02-19 17:54:39 +0900

    :loud_sound: add logging

--- quant_candles/views/aggregate_candles.py
+++ quant_candles/views/aggregate_candles.py
@@ -1,15 +1,20 @@
+import logging
+
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from quant_candles.controllers import aggregate_candles
 from quant_candles.filters import CandleFilter
 from quant_candles.models import Candle
 from quant_candles.serializers import TimeAgoWithRetrySerializer
+from quant_candles.storage import convert_candle_cache_to_daily
+
+logger = logging.getLogger(__name__)
 
 
-class AggregateCandleView(APIView):
+class AggregateCandleView(ListAPIView):
     queryset = Candle.objects.filter(is_active=True).prefetch_related("symbols")
     filter_backends = (DjangoFilterBackend,)
     filterset_class = CandleFilter
@@ -32,8 +37,8 @@ class AggregateCandleView(APIView):
 
     def get(self, request: Request, *args, **kwargs) -> Response:
         """Get data for each symbol."""
-        for candle, timestamp_from, timestamp_to, retry in self.get_command_kwargs(
-            request
-        ):
+        for candle, timestamp_from, timestamp_to, retry in self.get_params(request):
+            logger.info("{candle}: starting...".format(**{"candle": str(candle)}))
             aggregate_candles(candle, timestamp_from, timestamp_to, retry)
+            convert_candle_cache_to_daily(candle)
         return Response({"ok": True})
--- quant_candles/views/aggregate_trades.py
+++ quant_candles/views/aggregate_trades.py
@@ -1,3 +1,5 @@
+import logging
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 from rest_framework.request import Request
@@ -9,6 +11,8 @@ from quant_candles.filters import SymbolFilter
 from quant_candles.models import Symbol
 from quant_candles.serializers import TimeAgoWithRetrySerializer
 from quant_candles.storage import convert_trade_data_to_hourly
+
+logger = logging.getLogger(__name__)
 
 
 class AggregateTradeDataView(ListAPIView):
@@ -35,6 +39,7 @@ class AggregateTradeDataView(ListAPIView):
     def get(self, request: Request, *args, **kwargs) -> Response:
         """Get data for each symbol."""
         for symbol, timestamp_from, timestamp_to, retry in self.get_params(request):
+            logger.info("{symbol}: starting...".format(**{"symbol": str(symbol)}))
             api(symbol, timestamp_from, timestamp_to, retry)
             convert_trade_data_to_hourly(symbol, timestamp_from, timestamp_to)
             aggregate_trade_summary(symbol, timestamp_from, timestamp_to, retry)
