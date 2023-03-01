5fca59cdb50b1c6a655573946e649aa3c881c311
Alex <globophobe@gmail.com>
2023-03-01 09:49:52 +0900

    :card_file_box: update migrations

--- quant_candles/migrations/0002_candlereadonlydata_timebasedcandle_and_more.py
+++ quant_candles/migrations/0002_candlereadonlydata_timebasedcandle_and_more.py
@@ -7,7 +7,6 @@ import quant_candles.models.base
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("contenttypes", "0002_remove_content_type_name"),
         ("quant_candles", "0001_initial"),
