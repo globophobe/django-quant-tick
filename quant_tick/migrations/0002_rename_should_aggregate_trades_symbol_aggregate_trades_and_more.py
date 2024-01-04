8065e3808abe36ecc500133cd68c7f8b64d58d2b
Alex <globophobe@gmail.com>
2024-01-04 11:44:44 +0900

    :card_file_box: update migrations

--- quant_tick/migrations/0002_rename_should_aggregate_trades_symbol_aggregate_trades_and_more.py
+++ quant_tick/migrations/0002_rename_should_aggregate_trades_symbol_aggregate_trades_and_more.py
@@ -2,41 +2,165 @@
 
 from django.db import migrations, models
 
+import quant_tick.models.candles
+import quant_tick.models.trades
+
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('quant_tick', '0001_squashed'),
+        ("quant_tick", "0001_squashed"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='symbol',
-            old_name='should_aggregate_trades',
-            new_name='aggregate_trades',
+            model_name="symbol",
+            old_name="should_aggregate_trades",
+            new_name="aggregate_trades",
         ),
         migrations.AlterUniqueTogether(
-            name='symbol',
+            name="symbol",
             unique_together=set(),
         ),
         migrations.RemoveField(
-            model_name='globalsymbol',
-            name='notional',
+            model_name="globalsymbol",
+            name="notional",
         ),
         migrations.AddField(
-            model_name='symbol',
-            name='code_name',
-            field=models.SlugField(default='', max_length=255, unique=True, verbose_name='code name'),
+            model_name="symbol",
+            name="code_name",
+            field=models.SlugField(
+                default="", max_length=255, unique=True, verbose_name="code name"
+            ),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='symbol',
-            name='recent_error_at',
-            field=models.DateTimeField(help_text='If there was a recent API error, for example due to exchange maintenance, then backoff.', null=True, verbose_name='recent API error'),
+            model_name="symbol",
+            name="recent_error_at",
+            field=models.DateTimeField(
+                help_text="If there was a recent API error, for example due to exchange maintenance, then backoff.",
+                null=True,
+                verbose_name="recent API error",
+            ),
         ),
         migrations.AlterField(
-            model_name='symbol',
-            name='significant_trade_filter',
-            field=models.PositiveIntegerField(default=0, help_text='All trades less than the value of the significant trade filter will be aggregated, according to the the algorithm described in the documentation.', verbose_name='significant trade filter'),
+            model_name="symbol",
+            name="significant_trade_filter",
+            field=models.PositiveIntegerField(
+                default=0,
+                help_text="All trades less than the value of the significant trade filter will be aggregated, according to the the algorithm described in the documentation.",
+                verbose_name="significant trade filter",
+            ),
+        ),
+        migrations.DeleteModel(
+            name="CandleReadOnlyData",
+        ),
+        migrations.RenameField(
+            model_name="symbol",
+            old_name="aggregate_trades",
+            new_name="save_aggregated",
+        ),
+        migrations.RemoveField(
+            model_name="symbol",
+            name="currency_divisor",
+        ),
+        migrations.RenameField(
+            model_name="tradedata",
+            old_name="file_data",
+            new_name="filtered_data",
+        ),
+        migrations.AlterField(
+            model_name="tradedata",
+            name="filtered_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.trades.upload_filtered_data_to,
+                verbose_name="filtered data",
+            ),
+        ),
+        migrations.AddField(
+            model_name="candlecache",
+            name="file_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.candles.upload_candle_cache_data_to,
+                verbose_name="file data",
+            ),
+        ),
+        migrations.AddField(
+            model_name="symbol",
+            name="save_clustered",
+            field=models.BooleanField(
+                default=True,
+                help_text="Save clustered data?",
+                verbose_name="save clustered",
+            ),
+        ),
+        migrations.AddField(
+            model_name="symbol",
+            name="save_filtered",
+            field=models.BooleanField(
+                default=False,
+                help_text="Save filtered data?",
+                verbose_name="save filtered",
+            ),
+        ),
+        migrations.AddField(
+            model_name="symbol",
+            name="save_raw",
+            field=models.BooleanField(
+                default=False, help_text="Save raw data?", verbose_name="save raw"
+            ),
+        ),
+        migrations.AddField(
+            model_name="tradedata",
+            name="aggregated_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.trades.upload_aggregated_data_to,
+                verbose_name="aggregated data",
+            ),
+        ),
+        migrations.AddField(
+            model_name="tradedata",
+            name="candle_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.trades.upload_candle_data_to,
+                verbose_name="candle data",
+            ),
+        ),
+        migrations.AddField(
+            model_name="tradedata",
+            name="clustered_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.trades.upload_clustered_data_to,
+                verbose_name="clustered data",
+            ),
+        ),
+        migrations.AddField(
+            model_name="tradedata",
+            name="raw_data",
+            field=models.FileField(
+                blank=True,
+                upload_to=quant_tick.models.trades.upload_raw_data_to,
+                verbose_name="raw data",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="tradedata",
+            name="frequency",
+            field=models.PositiveIntegerField(
+                choices=[(1, "Minute"), (60, "Hour"), (1440, "Day")],
+                db_index=True,
+                verbose_name="frequency",
+            ),
+        ),
+        migrations.DeleteModel(
+            name="TradeDataSummary",
+        ),
+        migrations.AlterUniqueTogether(
+            name="tradedata",
+            unique_together={("symbol", "timestamp", "frequency")},
         ),
     ]
