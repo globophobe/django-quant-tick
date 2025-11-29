f6d030d763b239bedc1ac603974abaf62ad52ac9
Alex <globophobe@gmail.com>
2025-11-29 09:04:07 +0900

    :sparkles: add MLConfig to admin

--- demo/demo/admin.py
+++ demo/demo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from semantic_admin import SemanticModelAdmin, SemanticTabularInline
 
-from quant_tick.models import GlobalSymbol, Symbol
+from quant_tick.models import GlobalSymbol, MLConfig, Symbol
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
@@ -34,3 +34,33 @@ class SymbolAdmin(SemanticModelAdmin):
         ("symbol_type", "api_symbol"),
         ("aggregate_trades", "significant_trade_filter"),
     )
+
+
+@admin.register(MLConfig)
+class MLConfigAdmin(SemanticModelAdmin):
+    """MLConfig admin."""
+
+    list_display = ["code_name", "candle", "symbol", "horizon_bars", "is_active"]
+    list_filter = ["is_active", "candle", "symbol"]
+    search_fields = ["code_name"]
+
+    fieldsets = [
+        (None, {
+            "fields": ["candle", "symbol"]
+        }),
+        ("Training Parameters", {
+            "fields": ["horizon_bars", "inference_lookback"]
+        }),
+        ("Strategy Parameters", {
+            "fields": ["touch_tolerance", "min_hold_bars"]
+        }),
+        ("Configuration", {
+            "fields": ["json_data", "is_active"]
+        }),
+        ("Metadata", {
+            "fields": ["code_name", "last_processed_timestamp"],
+            "classes": ["collapse"]
+        }),
+    ]
+
+    readonly_fields = ["code_name", "last_processed_timestamp"]
