f04b3dabb5dcb518c7e96e563027b4a1dd1aac83
Alex <globophobe@gmail.com>
2026-04-05 11:01:10 +0900

    :fire: delete unnecessary code

--- quant_tick/serializers/__init__.py
+++ quant_tick/serializers/__init__.py
@@ -1,10 +1,7 @@
-from .candles import CandleDataSerializer, CandleSerializer
 from .timeago import TimeAgoSerializer, TimeAgoWithRetrySerializer
 from .timeframe import TimeFrameSerializer, TimeFrameWithLimitSerializer
 
 __all__ = [
-    "CandleDataSerializer",
-    "CandleSerializer",
     "TimeFrameSerializer",
     "TimeFrameWithLimitSerializer",
     "TimeAgoSerializer",
