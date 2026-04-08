a04959552690381da9a5cb9ef93b84214a8ef041
Alex <globophobe@gmail.com>
2026-04-08 09:10:23 +0900

    :sparkles: add compact view

--- quant_tick/views/__init__.py
+++ quant_tick/views/__init__.py
@@ -1,7 +1,9 @@
 from .aggregate_candles import AggregateCandleView
 from .aggregate_trades import AggregateTradeDataView
+from .compact import CompactView
 
 __all__ = [
     "AggregateCandleView",
     "AggregateTradeDataView",
+    "CompactView",
 ]
