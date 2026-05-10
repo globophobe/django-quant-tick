114ec0e0891a4be42584d76afee5b2d7094c5019
Alex <globophobe@gmail.com>
2026-05-10 12:06:27 +0900

    :white_check_mark: update workflow tests

--- quant_tick/tests/test_workflow.py
+++ quant_tick/tests/test_workflow.py
@@ -7,7 +7,7 @@ class WorkflowTest(SimpleTestCase):
     def test_workflow_calls_callback_before_compaction(self):
         workflow = get_workflow(
             "https://test.123/",
-            ["coinbase"],
+            [{"exchange": "coinbase", "api_symbol": "BTC-USD"}],
             callback_url="https://test.456/callback/",
             callback_interval_minutes=15,
         )
@@ -18,47 +18,69 @@ class WorkflowTest(SimpleTestCase):
                 "getRunTime",
                 "getTradeData",
                 "fetchExchangeData",
-                "aggregateCandles",
                 "maybeCallback",
                 "callback",
                 "compact",
             ],
         )
+        trade_steps = steps[1]["getTradeData"]["parallel"]["for"]["steps"][0][
+            "tradeAndCandleData"
+        ]["try"]["steps"]
         self.assertEqual(
-            steps[1]["getTradeData"]["parallel"]["for"]["steps"][0]["tradeData"][
-                "try"
-            ]["args"]["url"],
-            "${\"https://test.123/aggregate-trades/\" + exchange + \"/?time_ago=7d\"}",
+            trade_steps[0]["tradeData"]["args"]["url"],
+            "${item.url}",
+        )
+        self.assertEqual(trade_steps[0]["tradeData"]["result"], "tradeResponse")
+        self.assertEqual(
+            trade_steps[1]["candleData"]["args"]["url"],
+            "https://test.123/aggregate-candles/",
+        )
+        self.assertEqual(
+            trade_steps[1]["candleData"]["args"]["body"],
+            "${tradeResponse.body}",
         )
         self.assertEqual(
             steps[2]["fetchExchangeData"]["try"]["args"]["url"],
             "https://test.123/fetch-exchange-data/?time_ago=7d",
         )
         self.assertEqual(steps[2]["fetchExchangeData"]["except"]["steps"], [])
+        self.assertEqual(steps[3]["maybeCallback"]["next"], "compact")
+        self.assertEqual(steps[4]["callback"]["next"], "compact")
+        self.assertEqual(steps[4]["callback"]["args"]["body"], {"as_of": "${runTime}"})
         self.assertEqual(
-            steps[3]["aggregateCandles"]["args"]["url"],
-            "https://test.123/aggregate-candles/?time_ago=7d",
-        )
-        self.assertEqual(steps[4]["maybeCallback"]["next"], "compact")
-        self.assertEqual(steps[5]["callback"]["next"], "compact")
-        self.assertEqual(steps[5]["callback"]["args"]["body"], {"as_of": "${runTime}"})
-        self.assertEqual(
-            steps[6]["compact"]["args"]["url"],
+            steps[5]["compact"]["args"]["url"],
             "https://test.123/compact/?time_ago=7d",
         )
 
     def test_workflow_compacts_without_callback(self):
-        workflow = get_workflow("https://test.123/", ["coinbase"])
+        workflow = get_workflow(
+            "https://test.123/",
+            [{"exchange": "coinbase", "api_symbol": "BTC-USD"}],
+        )
         steps = workflow["main"]["steps"]
         self.assertEqual(
             [next(iter(step)) for step in steps],
-            ["getTradeData", "fetchExchangeData", "aggregateCandles", "compact"],
+            ["getTradeData", "fetchExchangeData", "compact"],
+        )
+        trade_steps = steps[0]["getTradeData"]["parallel"]["for"]["steps"][0][
+            "tradeAndCandleData"
+        ]["try"]["steps"]
+        self.assertEqual(
+            trade_steps[0]["tradeData"]["args"]["url"],
+            "${item.url}",
+        )
+        self.assertEqual(trade_steps[0]["tradeData"]["result"], "tradeResponse")
+        self.assertEqual(
+            trade_steps[1]["candleData"]["call"],
+            "http.post",
         )
         self.assertEqual(
-            steps[0]["getTradeData"]["parallel"]["for"]["steps"][0]["tradeData"][
-                "try"
-            ]["args"]["url"],
-            "${\"https://test.123/aggregate-trades/\" + exchange + \"/?time_ago=7d\"}",
+            trade_steps[1]["candleData"]["args"],
+            {
+                "url": "https://test.123/aggregate-candles/",
+                "auth": {"type": "OIDC"},
+                "body": "${tradeResponse.body}",
+            },
         )
         self.assertEqual(
             steps[1]["fetchExchangeData"]["try"]["args"]["url"],
@@ -66,10 +88,30 @@ class WorkflowTest(SimpleTestCase):
         )
         self.assertEqual(steps[1]["fetchExchangeData"]["except"]["steps"], [])
         self.assertEqual(
-            steps[2]["aggregateCandles"]["args"]["url"],
-            "https://test.123/aggregate-candles/?time_ago=7d",
-        )
-        self.assertEqual(
-            steps[3]["compact"]["args"]["url"],
+            steps[2]["compact"]["args"]["url"],
             "https://test.123/compact/?time_ago=7d",
+        )
+
+    def test_workflow_fanout_is_symbol_scoped(self):
+        workflow = get_workflow(
+            "https://test.123/",
+            [
+                {"exchange": "coinbase", "api_symbol": "BTC-USD"},
+                {"exchange": "binance-futures", "api_symbol": "BTC/USDT"},
+            ],
+        )
+        requests = workflow["main"]["steps"][0]["getTradeData"]["parallel"]["for"][
+            "in"
+        ]
+
+        self.assertEqual(
+            requests,
+            [
+                {
+                    "url": "https://test.123/aggregate-trades/coinbase/?time_ago=7d&api_symbol=BTC-USD"
+                },
+                {
+                    "url": "https://test.123/aggregate-trades/binance-futures/?time_ago=7d&api_symbol=BTC%2FUSDT"
+                },
+            ],
         )
